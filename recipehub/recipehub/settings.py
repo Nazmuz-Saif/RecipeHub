f14ff509fd1b3d65ac7bfa1ec4db50fed6eef660
Nazmuz-Saif <nazmuzsaif4080@gmail.com>
2026-05-10 02:01:29 +0600

    settings update

--- recipehub/recipehub/settings.py
+++ recipehub/recipehub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dth)2s_uha2ocfhgtnkoefiza*60-tmkg^bx4f0*l+3gj$j=vr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
